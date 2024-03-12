8ea47ff840da37ad936afb04f953bfb70f754324
test <andrey.mihaylovskiy@gmail.com>
2024-03-12 19:57:57 +0300

    Главная страница создана

---
+++ 1.js
@@ -0,0 +1,136 @@
+// let a;
+// let b;
+// a = +prompt('Введите число');
+// b = +prompt('Введите число');
+// console.log(`Summa ${++a + ++b}`);
+// console.log(`a = ${a}, b = ${b}`);
+
+// S = h * a * a
+
+
+//let a;
+
+//a = +prompt('Введите величину стороны основания');
+
+//let h;
+
+//h = +prompt('Введите величину высоты пирамиды')
+
+//let s = h * a * a;
+
+//console.log(`Площадь пирамиды со стороной ${a} и высотой ${h} равна ${s}`);
+
+
+// let a = 20;
+// let b = 30;
+// console.log(a > b);
+// console.log(a * 4 > b);
+// console.log(a > 100 || b < 50);
+// console.log(a*2 < 30 && b/10 == 3);
+
+// let year = 2020;
+// console.log(year % 400 == 0 || (year%100!=0 && year%4 == 0))
+
+// console.log((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) //правильный вариант
+
+// домашнее задание 1
+
+// console.log('Андрей')
+// console.log('Сергей')
+// console.log('Артем')
+
+// let a;
+// console.log(prompt('Введите строку'))
+
+
+// console.log(4*5+6-5/(23-11));
+
+
+// let x = 10;
+// let y = 10;
+// console.log(x>=y); 
+// console.log(++x>y); 
+// console.log(x<5 || y>100);
+// console.log(x<50 && y>100);
+// console.log(x>20 && y<20);
+// console.log((x<10 || y>50) && (++x==11 && ++y<=10));
+
+// let userName = prompt("Давай знакомиться");
+// userName=!Boolean(userName) ? "Иван":userName;
+
+// console.log(`Привет, ${userName}, рад с тобой познакомиться`)
+
+//  let S = prompt("Введите число от 1 до 3")
+//  switch (S){
+//     case "1" : alert("Маловато"); break;
+//     case "2" : alert("В самый раз"); break;
+//     case "3" : alert("Многовато"); break;
+//     default : alert("Ндааа");break;
+//  }
+
+// let pricePriz, sumMoney, choice;
+// let win = false;
+// pricePriz = Math.round(Math.random()*10000);
+// sumMoney = Math.round(Math.random()*10000);
+// console.log(`Приз стоит ${pricePriz}, денег положили ${sumMoney}`)
+// choice = prompt("1 - приз, 2 - деньги");
+// if(choice == 1){
+//     console.log(`Ваш приз стоит ${pricePriz}, а денег в шкатулке было ${sumMoney}`)
+//     if(pricePriz>=sumMoney) win = true; 
+
+// }
+// else if(choice == 2){
+//     console.log(`Сумма Ваших денег равна ${sumMoney}, а стоимость Приза была ${pricePriz}`)
+//     if (pricePriz<sumMoney) win = true;
+// }
+// else 
+// console.log("Вы удаляетесь из игры за несоблюдение правил")
+// if(win) console.log("Выйграл");
+// else console.log("Проиграл");
+
+// let ball;
+
+// ball = Math.round(Math.random()*10)
+// console.log(`Ученик правильно ответил на ${ball} вопросов теста`)
+
+// if (ball == 10) console.log("Оценка ученика 5 баллов")
+
+//     else if  (5<=ball && ball<=7) console.log("Оценка ученика 3 балла")
+//     else if (7<ball && ball<10) console.log("Оценка ученика 4 балла")
+//     else if (ball<5) console.log("Ученик не сдал тест")
+
+// let a,b,c,d; 
+// a = prompt("Введите первую сторону четырехугольника");
+// b = prompt("Введите вторую сторону четырехугольника");
+// c = prompt("Введите третью сторону четырехугольника");
+// d = prompt("Введите четвертую сторону четырехугольника");
+
+// if (a == c && b == d || a == d && b == c || a == b && d == c ) console.log ("Параллелограмм")
+// else console.log ("Просто четырехугольник") 
+
+// Задание 2 
+
+// let userNumber = prompt("Введите число");
+
+// if (isNaN(userNumber)){
+//     alert("Это не число! Попробуйте снова.")
+//     userNumber = prompt("Введите число")
+//     } else {
+//     alert("Спасибо!")
+//     }
+    // }if else {userNumber<0
+    //     console.log("Отрицательное число")
+    // }
+
+
+// for (let i = 0;i<11;i++){
+//     console.log("You are welcome")
+//     console.log(" ")    
+// }
+
+
+
+
+
+
+
---
+++ functions.js
@@ -0,0 +1,12 @@
+// function isCorrect(a,b){
+//     if (isNaN(a) || isNaN(b) || a<=0 || b<=0)
+//     return false
+//     else
+// }
+
+let sayHi = function() {
+    alert("Привет");
+}
+alert(sayHi); // выведет код функции
+let ups = sayHi;
+ups();
